349e833730e9496fa44b52b08c28e292e3bb4741
Edward Garrett <edward@pinedrop.com>
2015-01-11 17:18:41 +0000

    removed speaker chooser from tier selector

--- js/tier_selector.js
+++ js/tier_selector.js
@@ -1,11 +1,11 @@
 (function($) {
-        Drupal.behaviors.transcriptOptions = {
+        Drupal.behaviors.tierSelector = {
                 attach: function(context, settings) {
-                        $('[data-transcripts-role=transcript-controls]', context).once('options').each(function() {
+                        $('[data-transcripts-role=transcript-controls]', context).once('tier-selector').each(function() {
                                 var trid = $(this).attr('data-transcripts-id');
 				
 				var $tierSelector = $('.tier-selector', this);
-                                $tierSelector.find('optgroup[data-type=tier-options] option').attr('selected', true);
+                                $tierSelector.find('option').attr('selected', true);
                                 $tierSelector.change(function(e)
                                         {
                                                 $('*[data-transcripts-id=' + trid + ']').find('.tier').removeClass('active');
@@ -17,7 +17,7 @@
                                 );
 
                                 //hide buttons for tiers that have no data
-                                $('optgroup[data-type=tier-options] option', $tierSelector).each(function() {
+                                $('option', $tierSelector).each(function() {
                                         if ($('*[data-transcripts-id=' + trid + ']').find('*[data-tier=' + $(this).val() + ']').size() == 0) {
 						$(this).remove();                                                
                                         }
