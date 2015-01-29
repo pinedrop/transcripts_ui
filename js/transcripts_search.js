49d3726190c55ce671e2b233c8011fa7880887e1
Edward Garrett <edward@pinedrop.com>
2015-01-29 02:00:03 +0000

    new search cancels old search

---
+++ js/transcripts_search.js
@@ -0,0 +1,20 @@
+/**
+ * Created by edwardjgarrett on 1/29/15.
+ */
+(function ($) {
+    Drupal.behaviors.transcriptsSearch = {
+        attach: function (context, settings) {
+            $('[data-transcripts-role=transcript-controls]', context).once('transcripts-search').each(function () {
+                var trid = $(this).attr('data-transcripts-id');
+                var $scroller = ScrollingTranscript.getUI($('[data-transcripts-role=transcript][data-transcripts-id=' + trid + ']'));
+
+                var $search = $('.transcripts-ui-search-form', this);
+                // Overwrite beforeSubmit
+                Drupal.ajax['search-transcript-button-' + trid].options.beforeSubmit = function (form_values, element, options) {
+                    $scroller.clearHits();
+                }
+            });
+        }
+    };
+})(jQuery);
+
