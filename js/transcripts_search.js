922c5279cafc0b45b5eb1899111449738452ef7d
Edward Garrett <edward@pinedrop.com>
2015-01-29 02:39:17 +0000

    fieldset around search form

--- js/transcripts_search.js
+++ js/transcripts_search.js
@@ -9,6 +9,10 @@
                 var $scroller = ScrollingTranscript.getUI($('[data-transcripts-role=transcript][data-transcripts-id=' + trid + ']'));
 
                 var $search = $('.transcripts-ui-search-form', this);
+                $('.searchreset', $search).click(function () {
+                    $scroller.clearHits();
+                });
+
                 // Overwrite beforeSubmit
                 Drupal.ajax['searchbutton-transcript-' + trid].options.beforeSubmit = function (form_values, element, options) {
                     $scroller.clearHits();
