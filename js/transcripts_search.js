06e34ace1b3af5f07241e453dcb6753ed7c5dc56
Edward Garrett <edward@pinedrop.com>
2015-01-30 02:22:48 +0000

    scrolling to hits

--- js/transcripts_search.js
+++ js/transcripts_search.js
@@ -10,33 +10,33 @@
         var trid = $(this).attr('data-transcripts-id');
         var $transcript = $('[data-transcripts-role=transcript][data-transcripts-id=' + trid + ']');
         var $scroller = ScrollingTranscript.getUI($transcript);
-
-        $('.searchreset', this).click(function () {
-            $scroller.clearHits();
-        });
+        var hitCount = $('#transcript-results-count-' + trid).attr('data-results-count');
+        var hitIndex = 1;
+        var scrollHit = function(i) {
+            var $tcu = $('li[data-tcuid]:has(.hit):eq(' + (i-1) + ')');
+            $scroller.endAll();
+            $scroller.setOne($tcu);
+            $scroller.container.scrollTo($tcu);
+            $('#transcript-results-count-' + trid).html(i + ' of ' + hitCount);
+        };
 
         $('input:radio[name=transcript-search-options]', this).click(function() {
             if ($(this).attr('data-value') == 0) {
                 $('li[data-tcuid]', $transcript).show();
+                if (hitCount > 0) {
+                    scrollHit(hitIndex);
+                }
             }
             else {
                 $('li[data-tcuid]:not(:has(.hit))').hide();
+                if (hitCount > 0) {
+                    scrollHit(hitIndex);
+                }
             }
         });
         $('input:radio[name=transcript-search-options]', this).first().click();
-
-        var hitCount = $('#transcript-results-count-' + trid).attr('data-results-count');
-        if (hitCount > 0) {;
-            var scrollHit = function(i) {
-                var $tcu = $('li[data-tcuid]:has(.hit):eq(' + (i-1) + ')');
-                $scroller.endAll();
-                $scroller.setOne($tcu);
-                $scroller.container.scrollTo($tcu);
-                $('#transcript-results-count-' + trid).html(i + ' of ' + hitCount);
-            };
-            var hitIndex = 1;
-            scrollHit(hitIndex);
-
+;
+        if (hitCount > 0) {
             $('#transcript-nextresult-' + trid).click(function() {
                 if (hitIndex < hitCount) {
                     hitIndex++;
@@ -49,9 +49,18 @@
                     scrollHit(hitIndex);
                 }
             });
+
+            scrollHit(hitIndex);
         }
+
+        var $form = $(this);
+        $('#transcript-reset-button-' + trid).click(function () {
+            $form.removeClass('has-searched');
+            $scroller.clearHits();
+        });
+
         // Overwrite beforeSubmit
-        Drupal.ajax['searchbutton-transcript-' + trid].options.beforeSubmit = function (form_values, element, options) {
+        Drupal.ajax['transcript-search-button-' + trid].options.beforeSubmit = function (form_values, element, options) {
             $scroller.clearHits();
         };
 
