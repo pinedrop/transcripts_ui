622b7c4b07a84c896a595f181b244020f720a2bb
Edward Garrett <edward@pinedrop.com>
2015-02-04 14:29:17 +0000

    ajax progress plus incorporated cancel button code

--- js/transcripts_search.js
+++ js/transcripts_search.js
@@ -4,27 +4,27 @@
 (function ($) {
 
     //http://drupal.stackexchange.com/questions/79521/invoke-custom-js-function-in-ajax-callback
-    $.fn.transcriptSearchForm = function() {
-        $(this).addClass('transcript-search-processed');
+    $.fn.transcriptSearchForm = function () {
+        this.addClass('transcript-search-processed');
 
-        var trid = $(this).attr('data-transcripts-id');
+        var trid = this.attr('data-transcripts-id');
         var $transcript = $('[data-transcripts-role=transcript][data-transcripts-id=' + trid + ']');
         var $scroller = ScrollingTranscript.getUI($transcript);
         var hitCount = $('#transcript-results-count-' + trid).attr('data-results-count');
         var hitIndex = 1;
-        var scrollHit = function(i) {
-            var $tcu = $('li[data-tcuid]:has(.hit):eq(' + (i-1) + ')');
+        var scrollHit = function (i) {
+            var $tcu = $('li[data-tcuid]:has(.hit):eq(' + (i - 1) + ')');
             $scroller.endAll();
             $scroller.setOne($tcu);
             $scroller.container.scrollTo($tcu);
             $('#transcript-results-count-' + trid).html(i + ' of ' + hitCount);
         };
 
-        var clearHits = function() {
+        var clearHits = function () {
             $('.hit', $transcript).removeClass('hit').find('mark').contents().unwrap();
         };
 
-        $('input:radio[name=transcript-search-options]', this).click(function() {
+        $('input:radio[name=transcript-search-options]', this).click(function () {
             if ($(this).attr('data-value') == 0) {
                 $('li[data-tcuid]', $transcript).show();
                 if (hitCount > 0) {
@@ -39,15 +39,15 @@
             }
         });
         $('input:radio[name=transcript-search-options]', this).first().click();
-;
+
         if (hitCount > 0) {
-            $('#transcript-nextresult-' + trid).click(function() {
+            $('#transcript-nextresult-' + trid).click(function () {
                 if (hitIndex < hitCount) {
                     hitIndex++;
                     scrollHit(hitIndex);
                 }
             });
-            $('#transcript-previousresult-' + trid).click(function() {
+            $('#transcript-previousresult-' + trid).click(function () {
                 if (hitIndex > 1) {
                     hitIndex--;
                     scrollHit(hitIndex);
@@ -57,7 +57,7 @@
             scrollHit(hitIndex);
         }
 
-        var $form = $(this);
+        var $form = this;
         $('input[name=keys]', $form).val("");
         $('#transcript-reset-button-' + trid).click(function (e) {
             $('li[data-tcuid]', $transcript).show(); //show entire transcript
@@ -72,6 +72,30 @@
             clearHits();
             $scroller.endAll();
         };
+
+        var mbsrch = $('.form-text.form-control', this);  // the main search input
+        $(mbsrch).data("holder", $(mbsrch).attr("placeholder"));
+
+        // --- focusin - focusout
+        $(mbsrch).focusin(function () {
+            $(mbsrch).attr("placeholder", "");
+            $("button.searchreset").show("fast");
+        });
+        $(mbsrch).focusout(function () {
+            $(mbsrch).attr("placeholder", $(mbsrch).data("holder"));
+            $("button.searchreset").hide();
+
+            var str = "Enter Search...";
+            var txt = $(mbsrch).val();
+
+            if (str.indexOf(txt) > -1) {
+                $("button.searchreset").hide();
+                return true;
+            } else {
+                $("button.searchreset").show(100);
+                return false;
+            }
+        });
 
         return this;
     };
