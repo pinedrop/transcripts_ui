c5ca8a1f963f60916fb3a37ee0f543e3c984d56e
Edward Garrett <edward@pinedrop.com>
2015-02-17 00:07:30 +0000

    investigating ajax

--- js/transcripts_search.js
+++ js/transcripts_search.js
@@ -7,6 +7,8 @@
     Drupal.behaviors.transcriptSearch = {
         attach: function (context, settings) {
             $('[data-transcripts-role=transcript-search]', context).addBack('[data-transcripts-role=transcript-search]').once('transcript-search').each(function () {
+                console.log('settings');
+                console.log(settings);
                 console.log('attaching behaviors to transcript search');
                 console.log(this);
 
