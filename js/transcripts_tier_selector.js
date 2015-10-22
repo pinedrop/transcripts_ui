3a0e9d717914884cbcc5d541bfb53bb19816f366
Edward Garrett <edward@pinedrop.com>
2015-10-22 09:47:47 +0100

    removed unused active class from tiers and speaker names

--- js/transcripts_tier_selector.js
+++ js/transcripts_tier_selector.js
@@ -12,15 +12,15 @@
                     $tierSelector.find('optgroup[data-type=speakers] option').attr('selected', true)
                     $tierSelector.change(function (e) {
                             //language selection
-                            $('*[data-transcripts-id=' + trid + ']').find('.tier').hide(); //removeClass('active');
+                            $('*[data-transcripts-id=' + trid + ']').find('.tier').hide();
                             $('optgroup[data-type=languages] option:selected', this).each(function () {
-                                $('*[data-transcripts-id=' + trid + ']').find('*[data-tier=' + $(this).val() + ']').show(); //addClass('active');
+                                $('*[data-transcripts-id=' + trid + ']').find('*[data-tier=' + $(this).val() + ']').show();
                             });
 
                             //speaker name selection
-                            $('*[data-transcripts-id=' + trid + ']').find('.speaker-display').hide(); //removeClass('active');
+                            $('*[data-transcripts-id=' + trid + ']').find('.speaker-display').hide();
                             $('optgroup[data-type=speakers] option:selected', this).each(function () {
-                                $('*[data-transcripts-id=' + trid + ']').find('*[data-speaker-display=' + $(this).val() + ']').show(); //addClass('active');
+                                $('*[data-transcripts-id=' + trid + ']').find('*[data-speaker-display=' + $(this).val() + ']').show();
                             });
                             e.preventDefault();
                         }
